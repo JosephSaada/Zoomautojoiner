926c90ca5633d5cfbcdc1e06ac72cb2ede1451d8
Joseph Saada <64779012+JosephSaada@users.noreply.github.com>
2020-10-12 21:44:47 -0700

    Update data.py

--- data.py
+++ data.py
@@ -7,3 +7,14 @@ lst = [
     ["meeting link", "start time", "end time"] 
 ]
 # each line is spaced so an E501 error doesn't occur
+# example:
+# lst = [
+#  ["https://fjuhsd-org.zoom.us/j/8435842039?pwd=SVFJU0NCSDRSVHVzd3hCN0toQkFQZz09", "8:00", "8:51"],
+#  ["https://fjuhsd-org.zoom.us/j/85051704768?pwd=K1MvYWlJOUovK0s2eS84RTUxVUR5dz09", "8:59", "9:50"],
+# ["https://fjuhsd-org.zoom.us/j/82435141568?pwd=Nm9jUDJOTGcyTkhjZUIrcVdCMzZJdz09", "10:13", "11:04"],
+# ["https://meet.google.com/lookup/dby7jacj4k?authuser=0&hs=179", "11:12", "12:03"], 
+# ["https://us02web.zoom.us/j/9367980300?pwd=TGFqQjh5ZGlQaU83cDc5M0Jvdzd6UT09", "13:11", "14:02"],
+# ["https://bit.ly/2GTMuNz", "14:10", "15:01"]
+# ] 
+
+# integration with google meet is still being worked on, so if someone can do a pull request that'd be great
