fb124039c49b9066598a875386fa4ecf5ea85c23
Joseph Saada <64779012+JosephSaada@users.noreply.github.com>
2020-10-12 21:33:21 -0700

    Update data.py

--- data.py
+++ data.py
@@ -6,4 +6,4 @@ lst = [
     ["meeting link","start time", "end time"],   
     ["meeting link","start time", "end time"] 
 ]
-# each line is space so an E501 error doesn't occur
+# each line is spaced so an E501 error doesn't occur
