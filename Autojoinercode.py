47deee8eb8e97b5f6f979a9f47dda5ebad0557ba
Joseph Saada <64779012+JosephSaada@users.noreply.github.com>
2020-10-13 12:01:05 -0700

    Update Autojoinercode.py
    fixed issues when switching between periods, updated 2d array indexes to match those of the data.py

--- Autojoinercode.py
+++ Autojoinercode.py
@@ -1,23 +1,25 @@
 import time
 from datetime import datetime
-from pynput.keyboard import Controller, Key
+from pynput.keyboard import Controller
 from data import lst
 import webbrowser
 
 isStarted = False
 keyboard = Controller()
+classperiod = int(input("Enter school period to start at: "))
+classperiod -= 1
 
 for i in lst:
     while True:
         if not isStarted:
-            if datetime.now().hour == int(i[1].split(':')[0]) and datetime.now().minute == int(i[1].split(':')[1]):
-                webbrowser.open(i[0])
+            if datetime.now() == (lst[classperiod][1]):
+                webbrowser.open(lst[classperiod][0])
                 isStarted = True
         elif isStarted:
-            if datetime.now().hour == int(i[2].split(':')[0]) and datetime.now().minute == int(i[2].split(':')[1]):
-                keyboard.press('*')   
-# put in keybind for "end meeting"
+            if datetime.now() == (lst[classperiod][2]):
+                keyboard.press('-')
+                # put in keybind for "end meeting"
                 time.sleep(1)
-                keyboard.press(Key.enter)
-                isStarted = False 
+                isStarted = False
+                classperiod += 1
                 break
