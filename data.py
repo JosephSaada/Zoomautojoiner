0e464cf9b9efeb90af7b282eb47620812e73e003
Joseph Saada <64779012+JosephSaada@users.noreply.github.com>
2020-10-12 21:32:59 -0700

    Update data.py

--- data.py
+++ data.py
@@ -1,3 +1,9 @@
 lst = [
-    ["meeting link","start time", "end time"],   ["meeting link","start time", "end time"],   ["meeting link","start time", "end time"],   ["meeting link","start time", "end time"],   ["meeting link","start time", "end time"],   ["meeting link","start time", "end time"]
+    ["meeting link","start time", "end time"],   
+    ["meeting link","start time", "end time"],   
+    ["meeting link","start time", "end time"],   
+    ["meeting link","start time", "end time"],   
+    ["meeting link","start time", "end time"],   
+    ["meeting link","start time", "end time"] 
 ]
+# each line is space so an E501 error doesn't occur
